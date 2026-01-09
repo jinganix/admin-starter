8512dd3401fd8e0c49f7a2f28e927de0498ef103
gan.jin <jinganix@gmail.com>
2026-01-09 10:52:51 +0800

    refactor: disable redis (#25)

--- service/admin-starter/src/main/java/io/github/jinganix/admin/starter/helper/actor/OrderedTraceExecutor.java
+++ service/admin-starter/src/main/java/io/github/jinganix/admin/starter/helper/actor/OrderedTraceExecutor.java
@@ -2,8 +2,10 @@ package io.github.jinganix.admin.starter.helper.actor;
 
 import io.github.jinganix.peashooter.executor.DefaultExecutorSelector;
 import io.github.jinganix.peashooter.executor.TraceExecutor;
+import io.github.jinganix.peashooter.queue.CaffeineTaskQueueProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -11,9 +13,12 @@ import org.springframework.stereotype.Component;
 public class OrderedTraceExecutor
     extends io.github.jinganix.peashooter.executor.OrderedTraceExecutor {
 
-  public OrderedTraceExecutor(TraceExecutor executor, RedissonClient redissonClient) {
+  public OrderedTraceExecutor(
+      TraceExecutor executor, @Autowired(required = false) RedissonClient redissonClient) {
     super(
-        new DistributedTaskQueueProvider(redissonClient),
+        redissonClient == null
+            ? new CaffeineTaskQueueProvider()
+            : new DistributedTaskQueueProvider(redissonClient),
         new DefaultExecutorSelector(executor),
         executor.getTracer());
   }
